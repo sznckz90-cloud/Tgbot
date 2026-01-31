4c274519611b7314e258b0fc4b5ed4f04b915f81
sznckz90-cloud <sznckz90@gmail.com>
2026-01-31 12:26:00 +0530

    Add files via upload

---
+++ client/src/App.tsx
@@ -0,0 +1,33 @@
+import { Switch, Route } from "wouter";
+import { queryClient } from "./lib/queryClient";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "@/components/ui/toaster";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import NotFound from "@/pages/not-found";
+import Dashboard from "@/pages/Dashboard";
+import Users from "@/pages/Users";
+import Withdrawals from "@/pages/Withdrawals";
+
+function Router() {
+  return (
+    <Switch>
+      <Route path="/" component={Dashboard} />
+      <Route path="/users" component={Users} />
+      <Route path="/withdrawals" component={Withdrawals} />
+      <Route component={NotFound} />
+    </Switch>
+  );
+}
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        <Router />
+        <Toaster />
+      </TooltipProvider>
+    </QueryClientProvider>
+  );
+}
+
+export default App;
